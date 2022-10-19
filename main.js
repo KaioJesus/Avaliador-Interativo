9eca2f4322274b3589e7bfa9169cd052153ea3c7
Kaio Jesus <kaioramos990@hotmail.com>
2022-10-18 23:05:29 -0300

    add js not working and atribute 'onclick' in html for while

---
+++ main.js
@@ -0,0 +1,8 @@
+function rate(){
+    window.location.href = "rated.html"
+}
+
+const botaoEl = document.querySelector('#submitt')
+
+botaoEl.onclick = rate;
+// atrela uma função ao evento de "click" do botão
