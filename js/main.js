154b74142b42cdb2bbf1351aff4f802e77b06fab
Kaio Jesus <kaioramos990@hotmail.com>
2022-10-21 23:34:25 -0300

    improvements but ins't ready

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
-function rate(){
-    window.location.href = "rated.html"
-}
+const tela1 = document.querySelector('.card');
+const tela2 = document.querySelector('.card-2');
+const radioButton = document.querySelectorAll('.rating__radio');
 
-const botaoEl = document.querySelector('#submitt')
-
-botaoEl.onclick = rate;
+function onSubmit() {
+    tela1.style.display = 'none';
+    tela2.style.display = 'block';
+  };
+ 
+  function radioSelect(){
+    if(radioButton.onclick)
+  }
 // atrela uma função ao evento de "click" do botão
